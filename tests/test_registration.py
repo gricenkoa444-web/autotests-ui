3813e93a5a0f373fab3cdb1175065d60a628146e
gricenkoa444-web <gricenkoa444@gmail.com>
2026-01-31 16:25:42 +0300

    Интеграция Pytest and Playwright

---
+++ tests/test_registration.py
@@ -0,0 +1,34 @@
+#для того что бы запустить тестовый файл playwright_registration_one - его нужно обернуть в функцию
+from playwright.sync_api import sync_playwright
+
+
+def test_successful_registration():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+        registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        registration_email_input.fill('user.name_test@gmail.com')
+
+        registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        registration_username_input.fill('Username')
+
+        registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        registration_password_input.fill('Password')
+
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.click()
+
+        context.storage_state(path="one_browser_state.json")
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state="one_browser_state.json")
+        page = context.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/dashboard')
+
+        page.wait_for_timeout(5000)
