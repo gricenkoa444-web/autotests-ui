7cd0d4b0b37d2ccc002278ec29e60ae7154867cb
gricenkoa444-web <gricenkoa444@gmail.com>
2026-01-31 14:01:20 +0300

    Введение в pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,15 @@
+def test_user_login():
+    print("Hello user")
+
+class TestUserlogin:
+    def test_1(self):
+        ...
+
+    def test_2(self):
+        ...
+
+def test_assert_positive_case():
+    assert 1==1
+
+def test_assert_negative_case():
+    assert -1==1, "-1!=1"
