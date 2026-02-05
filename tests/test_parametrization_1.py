fbe2bbaf6b26e3e915f8a0b58443f133b968dccf
gricenkoa444-web <gricenkoa444@gmail.com>
2026-02-05 17:26:16 +0300

    Параметразация pytest

--- tests/test_parametrization_1.py
+++ tests/test_parametrization_1.py
@@ -1,4 +1,6 @@
 import pytest
+from _pytest.fixtures import SubRequest
+
 
 @pytest.mark.parametrize('number', [1, 2, 3, 4, -1])
 def test_numbers(number: int):
@@ -15,6 +17,68 @@ def test_several_numbers(number: int, expected: int):
 def test_multiplication_of_numbers(os: str, browser: str):
     assert len(os + browser) > 0
 
+
+# Параметризация Фикстуры
+@pytest.fixture(params=['chromium', 'webkit', 'firefox'])
+def browser(request: SubRequest):
+    return request.param
+
+# Используем фикстуру в тесте
+
+def test_open_browser(browser: str):
+    print(f'Running test on browser: {browser}')
+
+# Используем параметрайз в классе
+@pytest.mark.parametrize('user', ['Alice', 'Bob'])
+class TestOperation:
+    #@pytest.mark.parametrize('user', ['Alice', 'Bob'])
+    @pytest.mark.parametrize('account', ['Credit card', 'Debit card'])
+    def test_user_with_operation(self, user: str, account: str):
+        ...
+
+    #@pytest.mark.parametrize('user', ['Alice', 'Bob'])
+    def test_user_without_operation(self, user: str):
+        ...
+
+# Идентификаторы параметрайз  ids=['...', '...']
+@pytest.mark.parametrize(
+    'phone_number',
+    ['+7900000000', '+79000887711'],
+    ids=[ # не изменяем
+         'user with money on bank account',
+         'user without money on bank account'
+    ]
+)
+def test_identifiers(phone_number: str):
+    ...
+
+
+# Дминамическое формирование индентификатора ( более емкий вариант кода )
+
+user ={
+    '+7900000000': 'user with money on bank account',
+    '+79000887711': 'user without money on bank account'
+}
+
+@pytest.mark.parametrize(
+    'phone_number',
+     user.keys(),
+    # Можем использовать функцию но это избыточно, пример ( см скрипт ниже format_phone_number)
+    #ids=format_phone_number # передаем саму функцию - работает так же как и лямбда
+
+    # используем контекстную функцию lambda
+    # можно передать либо функцию, либо список - что удобнее то и делаем
+    ids=lambda phone_number: f'{phone_number}: {user[phone_number]}'# Работает динамическое форматирование
+)
+
+def test_identifiers_1(phone_number: str):
+    print('Динамическое формирование идентификаторов')
+
+
+# Обычная функция
+
+def format_phone_number(phone_number: str): -> str:
+    return f'{phone_number}: {user[phone_number]}'
 
 
 
