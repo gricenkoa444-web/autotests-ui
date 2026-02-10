9fe35abf6332ea2a99eea894ef9972b282bd4d1e
gricenkoa444-web <gricenkoa444@gmail.com>
2026-02-10 12:14:53 +0300

    Создание класса страницы CoursesListPage

---
+++ pages/courses_list_page.py
@@ -0,0 +1,11 @@
+from pages.base_page import BasePage
+from playwright.sync_api import Page, expect
+
+
+class CoursesListPage(BasePage):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+
+
+
