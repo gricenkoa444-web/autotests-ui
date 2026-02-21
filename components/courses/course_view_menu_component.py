f3c71c454e6af2b0a0d819046e2a752766b186bc
gricenkoa444-web <gricenkoa444@gmail.com>
2026-02-21 13:29:30 +0300

    pagefactory в автотестах

--- components/courses/course_view_menu_component.py
+++ components/courses/course_view_menu_component.py
@@ -1,24 +1,25 @@
 from components.base_component import BaseComponent
 from playwright.sync_api import Page, expect
+from elements.button import Button
 
 
 class CourseViewMenuComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.menu_button = page.get_by_test_id('course-view-menu-button')
-        self.edit_item = page.get_by_test_id('course-view-edit-menu-item')
-        self.delete_item = page.get_by_test_id('course-view-delete-menu-item')
+        self.menu_button = Button(page, 'course-view-menu-button', 'Menu Button')
+        self.edit_item = Button(page, 'course-view-edit-menu-item', 'Edit Menu Item')
+        self.delete_item = Button(page,'course-view-delete-menu-item', 'Delete Menu Item')
 
     
     def click_edit(self, index: int):
-        self.menu_button.nth(index).click()
+        self.menu_button.click(nth=index)
 
-        expect(self.edit_item.nth(index)).to_be_visible()
-        self.edit_item.nth(index).click()
+        self.edit_item.check_visible(nth=index)
+        self.edit_item.click(nth=index)
 
     def click_delete(self, index: int):
-        self.menu_button.nth(index).click()
+        self.menu_button.click(nth=index)
 
-        expect(self.delete_item.nth(index)).to_be_visible()
-        self.delete_item.nth(index).click()
+        self.delete_item.check_visible(nth=index)
+        self.delete_item.click(nth=index)
